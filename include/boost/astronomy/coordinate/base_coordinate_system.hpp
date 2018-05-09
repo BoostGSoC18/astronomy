0a501c18cc56a3feee5f5cd4829dbb6e75483e20
Pranam <plashkari628@gmail.com>
2018-05-09 22:26:30 +0530

    base_coordinate_system created

---
+++ include/boost/astronomy/coordinate/base_coordinate_system.hpp
@@ -0,0 +1,46 @@
+#ifndef BOOOST_BASE_COORDINATE_SYSTEM_HPP
+#define BOOOST_BASE_COORDINATE_SYSTEM_HPP
+
+#include <string>
+
+#include <boost/units/systems/si.hpp>
+#include <boost/units/systems/angle/degrees.hpp>
+#include <boost/units/io.hpp>
+
+
+namespace boost 
+{
+    namespace astronomy
+    {
+        namespace coordinate
+        {
+            typedef boost::units::quantity<boost::units::degree::plane_angle> degree;
+            typedef boost::units::quantity<boost::units::divide_typeof_helper
+                <boost::units::degree::plane_angle,boost::units::si::time>::type>
+                degree_per_sec;
+
+            template <class Representation=void, class Differential=void>
+            struct base_coordinate_system
+            {
+            protected:
+                std::string system_name;
+                std::string representation;
+                std::string differential;
+
+            public:
+                template<class OtherRepresentation>
+                degree separation(base_coordinate_system<OtherRepresenation> other);
+
+                template<class System>
+                virtual bool is_transformable_to() = 0;
+
+                degree_per_sec motion();
+
+                degree_per_sec radial_velocity();
+
+
+            };
+        }
+    }
+}
+#endif  // !BOOOST_BASE_COORDINATE_SYSTEM_HPP
--- include/boost/astronomy/coordinate/sky_point.hpp
+++ include/boost/astronomy/coordinate/sky_point.hpp
@@ -2,9 +2,10 @@
 #define BOOST_SKY_POINT_HPP
 
 
-#include<string>
-#include<boost\units\base_units\angle\degree.hpp>
-#include<boost\units\base_units\angle\radian.hpp>
+#include <string>
+
+#include <boost/units/systems/angle/degrees.hpp>
+#include <boost/units/io.hpp>
 
 
 namespace boost
@@ -13,33 +14,56 @@ namespace boost
     {
         namespace coordinate
         {
-            typedef boost::units::angle::degree_base_unit degree;
+            typedef boost::units::quantity<boost::units::degree::plane_angle> degree;
 
             /*sky_point is used to represent a point(coordinate) in the sky*/
-            template <class System, typename Unit, typename Unit2 = void>
+            template <class System, typename Unit, typename Unit2=void>
             struct sky_point
             {
             protected:
-                degree latitude;
-                degree longitude;
-                std::string coordinate_system;
+                degree latitude;    //hosrizontal coordinate
+                degree longitude;   //vertical coordinate
+                std::string coordinate_system;  //type of coordinate system
 
             public:
-                // constructor  
+                //constructors  
+
+                //default constructor
                 sky_point() {}
+
+                //copy constructor
+                template<class OtherSystem, typename OtherUnit, typename OtherUnit2 = void>
+                sky_point(const sky_point<OtherSystem, OtherUnit, OtherUnit2> object);
+
+                //constructing from numbers
                 sky_point(double latitude, double longitude);
-                sky_point(std::string latitude, std::string longitude);
-                sky_point(std::string name);
+
+                //constructing from string
+                sky_point(const char* latitude, const char* longitude);
+
+                //constructing from name of object if available in the calatoge
+                sky_point(const char* name);
 
                 // destructor 
                 ~sky_point();
 
                 std::string get_constillation();
-                degree separation(sky_point<System, Unit, Unit2> object);
-                bool is_equivalant_system(sky_point<System, Unit, Unit2> object);
-                sky_point<System, Unit, Unit2> from_name(std::string name);
-                degree positional_angle(sky_point<System, Unit, Unit2> object);
-            };
+
+                template<class OtherSystem, typename OtherUnit, typename OtherUnit2 = void>
+                degree separation(const sky_point<OtherSystem, OtherUnit, OtherUnit2> object);
+
+                template<class OtherSystem, typename OtherUnit, typename OtherUnit2 = void>
+                bool is_equivalant_system(const sky_point<OtherSystem, OtherUnit, OtherUnit2> object);
+
+                sky_point<System, Unit, Unit2> from_name(const char* name);
+
+                template<class OtherSystem, typename OtherUnit, typename OtherUnit2 = void>
+                degree positional_angle(const sky_point<OtherSystem, OtherUnit, OtherUnit2> object);
+
+                template<class OtherSystem, typename OtherUnit, typename OtherUnit2 = void>
+                sky_point<OtherSystem, OtherUnit, OtherUnit2> transform_to();
+
+            }; //sky_point
 
         } // namespace coordinate
     } // namespace astronomy

db5015539ae37e5a795ec6b77d24407f8d4c0bea
Pranam <plashkari628@gmail.com>
2018-05-13 00:57:17 +0530

    definition of cross and dot product created cartesian_representation created

--- include/boost/astronomy/coordinate/base_representation.hpp
+++ include/boost/astronomy/coordinate/base_representation.hpp
@@ -3,49 +3,119 @@
 
 
 #include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/algorithms/transform.hpp>
+#include <boost/geometry/arithmetic/cross_product.hpp>
+#include <boost/geometry/arithmetic/dot_product.hpp>
+#include <boost/geometry/core/cs.hpp>
+#include <boost/type_traits/is_base_of.hpp>
+#include <boost/static_assert.hpp>
 
 namespace boost
 {
+    // structure to provide support like std::is_base_of for template base classes
+    template <template <typename...> class Base, typename Derived>
+    struct base_template
+    {
+        using U = typename std::remove_cv<Derived>::type;
+
+        template <typename... Args>
+        static std::true_type test(Base<Args...>*);
+
+        static std::false_type test(void*);
+
+        using type = decltype(test(std::declval<U*>()));
+    };
+
+    template <template <typename...> class Base, typename Derived>
+    using is_template_base_of = typename base_template<Base, Derived>::type;
+
     namespace astronomy
     {
         namespace coordinate
         {
-
+            // abstract structure which is the base for all the representation 
             template <int DimensionCount, typename Type>
-            struct base_representation
+            struct base_representation: public representation
             {
             protected:
-                protected boost::geometry::model::point<double, DimensionCount, Type> point;
+                boost::geometry::model::point<double, DimensionCount, Type> point;
             public:
                 
                 // cross prodct of current vector with specified vector
-                template<int OtherDimensionCount, typename OtherType, int ReturnDimensionCount = DimensionCount, typename ReturnType = Type>
-                virtual base_representation<ReturnDimensionCount, ReturnType>
-                    cross(const base_representation<OtherDimensionCount, OtherType> other) const = 0;
+                template <typename ReturnType, typename Representation>
+                virtual ReturnType cross(Representation const& other) const
+                {
+                    /*both the coordinates/vector are first converted into cartesian coordinate system then
+                    cross product of both cartesian vectors is converted into requested type and returned*/
 
+                    /*checking types of argument and return type*/
+                    BOOST_STATIC_ASSERT_MSG((boost::is_template_base_of
+                        <boost::astronomy::coordinate::base_representation, Representation>::value),
+                        "function argument type is expected to be a representation class");
+
+                    BOOST_STATIC_ASSERT_MSG((boost::is_template_base_of
+                        <boost::astronomy::coordinate::base_representation, ReturnType>::value), 
+                        "return type is expected to be a representation class");
+
+                    /*converting both coordinates/vector into cartesian system*/
+                    boost::geometry::model::point<double, 3, boost::geometry::cs::cartesian> tempPoint1;
+                    boost::geometry::transform(this->point, tempPoint1);
+
+                    boost::geometry::model::point<double, 3, boost::geometry::cs::cartesian> tempPoint2;
+                    boost::geometry::transform(other.get_point(), tempPoint2);
+
+                    /*creating object of the specified return type and returning it */
+                    return ReturnType(boost::geometry::cross_product
+                        <boost::geometry::model::point<double, 3, boost::geometry::cs::cartesian>>
+                        (tempPoint1, tempPoint2));
+                }
+                
                 // dot prodct of current vector with specified vector
-                template<int OtherDimensionCount, typename OtherType>
-                virtual double dot(const base_representation<OtherDimensionCount, OtherType> other) const = 0;
+                template <typename Representation>
+                virtual double dot(Representation const& other) const
+                {
+                    /*both the coordinates/vector are first converted into cartesian coordinate system then
+                    dot product of both cartesian product is converted into requested type and returned*/
+
+                    /*checking types of argument type*/
+                    BOOST_STATIC_ASSERT_MSG((boost::is_template_base_of
+                        <boost::astronomy::coordinate::base_representation, Representation>::value),
+                        "function argument type is expected to be a representation class");
+
+                    /*converting both coordinates/vector into cartesian system*/
+                    boost::geometry::model::point<double, 3, boost::geometry::cs::cartesian> tempPoint1;
+                    boost::geometry::transform(point, tempPoint1);
+
+                    boost::geometry::model::point<double, 3, boost::geometry::cs::cartesian> tempPoint2;
+                    boost::geometry::transform(other.get_point(), tempPoint2);
+
+                    return boost::geometry::dot_product(tempPoint1, tempPoint2);
+
+                }
 
                 // returns the unit vector of current vector
-                template<int OtherDimensionCount = DimensionCount, typename OtherType = Type>
-                virtual base_representation<OtherDimensionCount, OtherType> unit_vector() const = 0;
+                template <typename ReturnType>
+                virtual ReturnType unit_vector() const = 0;
 
                 // converts current representation into specified representation
-                template<int OtherDimensionCount, typename OtherType>
-                virtual base_representation<OtherDimensionCount, OtherType> to_representation() const = 0;
+                template <typename ReturnType>
+                virtual ReturnType to_representation() const = 0;
 
                 // sum of current vector and specified vector
-                template<int OtherDimensionCount, typename OtherType, int ReturnDimensionCount = DimensionCount, typename ReturnType = Type>
-                virtual base_representation<ReturnDimensionCount, ReturnType>
-                    sum(const base_representation<OtherDimensionCount, OtherType> other) const = 0;
+                template <typename ReturnType>
+                virtual ReturnType sum(representation const& other) const = 0;
 
                 // mean of current vector with specified vector
-                template<int OtherDimensionCount, typename OtherType>
-                virtual base_representation<ReturnDimensionCount, ReturnType>
-                    mean(const base_representation<OtherDimensionCount, OtherType> other) const = 0;
+                template <typename ReturnType>
+                virtual ReturnType mean(representation const& other) const = 0;
 
-                virtual double norm() = 0;
+                // norm of the current class is returned
+                virtual double norm() const = 0;
+
+                boost::geometry::model::point<double, DimensionCount, Type> get_point() const
+                {
+                    return this->point;
+                }
             };
         }
     }
---
+++ include/boost/astronomy/coordinate/cartesian_representation.hpp
@@ -0,0 +1,57 @@
+#ifndef CARTESIAN_REPRESENTATION_HPP
+#define CARTESIAN_REPRESENTATION_HPP
+
+
+#include <boost/geometry/core/cs.hpp>
+#include <boost/geometry/arithmetic/cross_product.hpp>
+#include <boost/geometry/arithmetic/dot_product.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/algorithms/transform.hpp>
+#include <boost/type_traits/is_base_of.hpp>
+#include <boost/static_assert.hpp>
+
+#include <boost/astronomy/coordinate/base_representation.hpp>
+
+
+namespace boost
+{
+    namespace astronomy
+    {
+        namespace coordinate
+        {
+            struct cartesian_representation : public base_representation<3, boost::geometry::cs::cartesian>
+            {
+            public:
+                cartesian_representation(double x, double y, double z=0.0)
+                {
+                    boost::geometry::set<0>(this->point, x);
+                    boost::geometry::set<1>(this->point, y);
+                    boost::geometry::set<2>(this->point, z);
+                }
+
+                template <int DimensionCount, typename Type>
+                cartesian_representation(boost::geometry::model::point<double, DimensionCount, Type> pointObject)
+                {
+                    boost::geometry::transform(pointObject, this->point);
+                }
+
+                cartesian_representation(cartesian_representation &object)
+                {
+                    this->point = object.get_point();
+                }
+
+                template <typename Representation>
+                cartesian_representation(Representation const& other)
+                {
+                    BOOST_STATIC_ASSERT_MSG((boost::is_template_base_of
+                        <boost::astronomy::coordinate::base_representation, Representation>::value),
+                        "No constructor found with given argument type");
+
+                    boost::geometry::transform(other.get_point(), this->point);
+                }
+            };
+        }
+    }
+}
+
+#endif // !CARTESIAN_REPRESENTATION_HPP

5941dc09460a64b89ff0d05f1e39fc06f36654ea
Pranam <plashkari628@gmail.com>
2018-06-18 00:26:18 +0530

    supergalactic_frame created

--- include/boost/astronomy/coordinate/supergalactic.hpp
+++ include/boost/astronomy/coordinate/supergalactic.hpp
@@ -1,17 +1,9 @@
-#ifndef BOOST_ASTRONOMY_COORDINATE_GALACTIC_FRAME_HPP
-#define BOOST_ASTRONOMY_COORDINATE_GALACTIC_FRAME_HPP
+#ifndef BOOST_ASTRONOMY_COORDINATE_SUPERGALACTIC_FRAME_HPP
+#define BOOST_ASTRONOMY_COORDINATE_SUPERGALACTIC_FRAME_HPP
 
-#include <boost/geometry/core/cs.hpp>
-#include <boost/geometry/geometries/point.hpp>
-#include <boost/geometry/algorithms/transform.hpp>
-#include <boost/geometry/algorithms/equals.hpp>
-
-#include <boost/is_base_template_of.hpp>
-#include <boost/astronomy/coordinate/base_representation.hpp>
 #include <boost/astronomy/coordinate/base_frame.hpp>
 #include <boost/astronomy/coordinate/spherical_representation.hpp>
 #include <boost/astronomy/coordinate/spherical_coslat_differential.hpp>
-
 namespace boost
 {
     namespace astronomy
@@ -20,17 +12,17 @@ namespace boost
         {
             template <typename RepresentationDegreeOrRadian = degree,
                 typename DifferentialDegreeOrRadian = degree>
-            struct galactic_frame: public boost::astronomy::coordinate::base_frame
+            struct supergalactic_frame : public boost::astronomy::coordinate::base_frame
                 <boost::astronomy::coordinate::spherical_representation<RepresentationDegreeOrRadian>,
                 boost::astronomy::coordinate::spherical_coslat_differential<DifferentialDegreeOrRadian>>
             {
             public:
                 //default constructor no initialization
-                galactic_frame() {}
+                supergalactic_frame() {}
 
-                //creates coordinate in galactic frame using any subclass of base_representation
+                //creates coordinate in supergalactic frame using any subclass of base_representation
                 template <typename Representation>
-                galactic_frame(Representation const& representation_data)
+                supergalactic_frame(Representation const& representation_data)
                 {
                     BOOST_STATIC_ASSERT_MSG((boost::is_base_template_of
                         <boost::astronomy::coordinate::base_representation, Representation>::value),
@@ -40,26 +32,26 @@ namespace boost
                 }
 
                 //creates coordinate from given values
-                //b -> latitude, l -> longitude
-                galactic_frame(double b, double l, double distance)
+                //sgb -> latitude, sgl -> longitude
+                supergalactic_frame(double sgb, double sgl, double distance)
                 {
-                    this->data.set_lat_lon_dist(b, l, distance);
+                    this->data.set_lat_lon_dist(sgb, sgl, distance);
                 }
 
                 //creates coordinate with motion from given values
-                //b -> latitude, l -> longitude
-                //pm_b -> proper motion in b, pm_l_cosb -> proper motion in l including cos(b) 
-                galactic_frame(double b, double l, double distance, double pm_b, double pm_l_cosb, double radial_velocity):
-                    galactic_frame(b, l, distance)
+                //sgb -> latitude, sgl -> longitude
+                //pm_sgb -> proper motion in sgb, pm_sgl_cossgb -> proper motion in sgl including cos(sgb) 
+                supergalactic_frame(double sgb, double sgl, double distance, double pm_sgb, double pm_sgl_cossgb, double radial_velocity) :
+                    supergalactic_frame(sgb, sgl, distance)
                 {
-                    this->motion.set_dlat_dlon_coslat_ddist(pm_b, pm_l_cosb, radial_velocity);
+                    this->motion.set_dlat_dlon_coslat_ddist(pm_sgb, pm_sgl_cossgb, radial_velocity);
                 }
 
                 //creates coordinate with motion
                 //representation class is used for coordinate data
                 //differential class is used for motion data
                 template <typename Representation, typename Differential>
-                galactic_frame(Representation const& representation_data, Differential const& diff)
+                supergalactic_frame(Representation const& representation_data, Differential const& diff)
                 {
                     BOOST_STATIC_ASSERT_MSG((boost::is_base_template_of
                         <boost::astronomy::coordinate::base_representation, Representation>::value),
@@ -73,38 +65,38 @@ namespace boost
                 }
 
                 //copy constructor
-                galactic_frame(galactic_frame const& other)
+                supergalactic_frame(supergalactic const& other)
                 {
                     this->data = other.get_data();
                     this->motion = other.get_differential();
                 }
 
-                //returns component b of the galactic coordinate
-                double get_b() const
+                //returns component sgb of the supergalactic coordinate
+                double get_sgb() const
                 {
                     return boost::geometry::get<0>(this->data.get_point());
                 }
 
-                //returns component l of the galactic coordinate
-                double get_l() const
+                //returns component sgl of the supergalactic coordinate
+                double get_sgl() const
                 {
                     return boost::geometry::get<1>(this->data.get_point());
                 }
 
-                //returns distance component of the galactic coordinate
+                //returns distance component of the supergalactic coordinate
                 double get_distance() const
                 {
                     return boost::geometry::get<2>(this->data.get_point());
                 }
 
-                //returns proper motion in galactic latitude
-                double get_pm_b() const
+                //returns proper motion in supergalactic latitude
+                double get_pm_sgb() const
                 {
                     return boost::geometry::get<0>(this->motion.get_differential());
                 }
 
-                //returns proper motion in galactic longitude including cos(b)
-                double get_pm_l_cosb() const
+                //returns proper motion in supergalactic longitude including cos(b)
+                double get_pm_sgl_cossgb() const
                 {
                     return boost::geometry::get<1>(this->motion.get_differential());
                 }
@@ -115,34 +107,34 @@ namespace boost
                     return boost::geometry::get<2>(this->motion.get_differential());
                 }
 
-                //sets value of component b of the galactic coordinate
-                double set_b(double b)
+                //sets value of component b of the supergalactic coordinate
+                double set_sgb(double b)
                 {
-                    boost::geometry::set<0>(this->data.get_point(), b);
+                    boost::geometry::set<0>(this->data.get_point(), sgb);
                 }
 
-                //sets value of component l of the galactic coordinate
-                double set_l(double l) const
+                //sets value of component sgl of the supergalactic coordinate
+                double set_sgl(double l) const
                 {
-                    boost::geometry::set<1>(this->data.get_point(), l);
+                    boost::geometry::set<1>(this->data.get_point(), sgl);
                 }
 
-                //sets value of distance component of the galactic coordinate
+                //sets value of distance component of the supergalactic coordinate
                 double set_distance(double distance)
                 {
                     boost::geometry::set<2>(this->data.get_point(), distance);
                 }
 
-                //sets the proper motion in galactic latitude
-                double set_pm_b(double pm_b)
+                //sets the proper motion in supergalactic latitude
+                double set_pm_sgb(double pm_sgb)
                 {
-                    boost::geometry::set<0>(this->motion.get_differential(), pm_b);
+                    boost::geometry::set<0>(this->motion.get_differential(), pm_sgb);
                 }
 
-                //sets the proper motion in galactic longitude including cos(b)
-                double set_pm_l_cosb(double pm_l_cosb)
+                //sets the proper motion in supergalactic longitude including cos(b)
+                double set_pm_sgl_cossgb(double pm_sgl_cossgb)
                 {
-                    boost::geometry::set<1>(this->motion.get_differential(), pm_l_cosb);
+                    boost::geometry::set<1>(this->motion.get_differential(), pm_sgl_cossgb);
                 }
 
                 //sets the radial_velocity
@@ -154,4 +146,5 @@ namespace boost
         } //namespace coordinate
     } //namespace astronomy
 } //namespace boost
-#endif // !BOOST_ASTRONOMY_COORDINATE_GALACTIC_FRAME_HPP
+#endif // !BOOST_ASTRONOMY_COORDINATE_SUPERGALACTIC_FRAME_HPP
+

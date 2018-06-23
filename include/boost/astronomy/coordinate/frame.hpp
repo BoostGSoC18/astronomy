86994dd0f215dd3aca4c34ddf0ff2931173d1d96
Pranam <plashkari628@gmail.com>
2018-06-23 12:25:54 +0530

    frame header file created

---
+++ include/boost/astronomy/coordinate/frame.hpp
@@ -0,0 +1,13 @@
+#ifndef BOOST_ASTRONOMY_COORDINATE_FRAME_HPP
+#define BOOST_ASTRONOMY_COORDINATE_FRAME_HPP  
+
+#include <boost/astronomy/coordinate/alt_az.hpp>
+#include <boost/astronomy/coordinate/cirs.hpp>
+#include <boost/astronomy/coordinate/galactic.hpp>
+#include <boost/astronomy/coordinate/geocentric.hpp>
+#include <boost/astronomy/coordinate/heliocentric.hpp>
+#include <boost/astronomy/coordinate/icrs.hpp>
+#include <boost/astronomy/coordinate/supergalactic.hpp>
+
+#endif // !BOOST_ASTRONOMY_COORDINATE_FRAME_HPP
+

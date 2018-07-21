d8e02de5508b3ccb26348cacc739d80c6e3d1c25
Pranam <plashkari628@gmail.com>
2018-07-21 23:41:45 +0530

    basic hdu structure built

--- include/boost/astronomy/io/fits.hpp
+++ include/boost/astronomy/io/fits.hpp
@@ -1,11 +1,7 @@
 #ifndef BOOST_ASTRONOMY_IO_FITS_HPP
 #define BOOST_ASTRONOMY_IO_FITS_HPP
 
-#include <map>
-#include <string>
-#include <fstream>
-
-#include <boost/algorithm/string/trim.hpp>
+#include<boost/astronomy/io/hdu.hpp>
 
 namespace boost
 {
@@ -15,30 +11,6 @@ namespace boost
         {
             struct fits {
             protected:
-                std::multimap<std::string, std::string> header;
-                std::fstream file;
-
-            public:
-                fits() {}
-
-                fits(std::string const& path, std::ios_base::openmode mode = std::ios_base::in)
-                {
-                    file.open(path, mode);
-                    
-                    char card[81] = "";
-                    std::string header_part = "", value_part = "";
-                    while (header_part != "END")
-                    {
-                        file.read(card, 80);
-                        header_part.assign(card, 8);
-                        boost::algorithm::trim(header_part);
-                        value_part.assign(card + 10, 70);
-                        if(header_part != "")
-                            header.insert(std::pair<std::string, std::string>(header_part, value_part));
-                    }
-
-                    file.seekg(((int)(file.tellg() / 2880) + 1) * 2880);
-                }
 
             };
         } //namespace io
---
+++ include/boost/astronomy/io/hdu.hpp
@@ -0,0 +1,143 @@
+#ifndef BOOST_ASTRONOMY_IO_HDU_HPP
+#define BOOST_ASTRONOMY_IO_HDU_HPP
+
+#include <map>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cstddef>
+
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <boost/astronomy/exception/fits_exception.hpp>
+
+namespace boost
+{
+    namespace astronomy
+    {
+        namespace io
+        {
+            //structure to store a card (80 byte key value pairs as well as comments and history cards)
+            struct card
+            {
+            private:
+                std::string card_;
+
+            public:
+                card()
+                {
+                    this->card_.reserve(80);
+                }
+
+                card(std::string str) : card()
+                {
+                    if (str.length() != 80)
+                    {
+                        throw invalid_card_length_exception();
+                    }
+                    this->card_ = str;
+                }
+
+                std::string keyword(bool whole = false) const
+                {
+                    if (whole)
+                    {
+                        return this->card_.substr(0, 8);
+                    }
+                    return boost::algorithm::trim_copy(this->card_.substr(0, 8));
+                }
+
+                template <typename ReturnType>
+                ReturnType value(bool comment = false) const
+                {
+                    if (comment)
+                    {
+                        return this->card_.substr(10);
+                    }
+                    std::string val = boost::algorithm::trim_copy(this->card_.substr(10, this->card_.find('/')));
+                    try
+                    {
+                        return boost::lexical_cast<int>(val);
+                    }
+                    catch (boost::bad_lexical_cast int_e)
+                    {
+                        try
+                        {
+                            return boost::lexical_cast<double>(val);
+                        }
+                        catch (boost::bad_lexical_cast double_e)
+                        {
+                            return val;
+                        }
+                    }
+
+                }
+
+                void value(std::string const& value)
+                {
+                    if (value.length() > 70)
+                    {
+                        throw invalid_card_length_exception();
+                    }
+                    this->card_.append(70 - value.length(), ' ');
+                }
+            };
+
+            //structure to represent a header unit with max 36 cards
+            struct hdu_unit
+            {
+            protected:
+                std::vector<card> cards;
+
+            public:
+                hdu_unit() {}
+
+                hdu_unit(std::vector<card> c)
+                {
+                    if (c.size() > 36)
+                    {
+                        throw hdu_unit_overflow_exception();
+                    }
+                    this->cards = c;
+                }
+
+                void add_card(card c)
+                {
+                    if (cards.size() == 36)
+                    {
+                        throw hdu_unit_overflow_exception();
+                    }
+                    this->cards.push_back(c);
+                }
+
+                void add_card(std::string str)
+                {
+                    card c(str);
+                    this->add_card(c);
+                }
+
+                inline std::size_t size()
+                {
+                    return cards.size();
+                }
+            };
+
+            struct hdu
+            {
+            protected:
+                long bitpix;
+                long naxis;
+                long bzero;
+                long bscale;
+
+            public:
+                hdu() {}
+
+            };
+        } //namespace io
+    } //namespace astronomy
+} //namespace boost
+
+#endif // !BOOST_ASTRONOMY_IO_HDU_HPP
+

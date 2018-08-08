deea0d83825fb86c249ba7f50069c643a4fa5fdb
Pranam <plashkari628@gmail.com>
2018-08-08 13:59:29 +0530

    coordinate.hpp

---
+++ include/boost/astronomy/coordinate/coordinate.hpp
@@ -0,0 +1,9 @@
+#ifndef BOOST_ASTRONOMY_COORDINATE_HPP
+#define BOOST_ASTRONOMY_COORDINATE_HPP
+
+#include <boost/astronomy/coordinate/representation.hpp>
+#include <boost/astronomy/coordinate/differential.hpp>
+#include <boost/astronomy/coordinate/frame.hpp>
+
+#endif // !BOOST_ASTRONOMY_COORDINATE_HPP
+
